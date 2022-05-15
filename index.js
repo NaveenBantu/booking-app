62a313f0bd78e3e3673d1210689fa0ef93875a55
Naveen Mac <naveen.bantu1@gmail.com>
2022-05-15 12:36:13 +0000

    Add MongoDB config and connection

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
 const app = express()
+dotenv.config()
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("connected to mongoDB")
+    } catch (error) {
+        throw error;
+    }   
+}
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("mongoDB is disconnected!")
+})
+
+mongoose.connection.on("connected", ()=>{
+    console.log("mongoDB is connected!")
+})
 
 app.listen(8800, () => {
+    connect()
     console.log("Connected to our backend!")
 })
